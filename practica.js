6c759fd0e12fd3944bcb5766c36157b2a68667d7
¨Angelica Cisneros <angie.cisneros.gomez@gmail.com>
2022-02-17 23:57:40 -0600

    practica js

--- practica.js
+++ practica.js
@@ -5,3 +5,12 @@ let a=89
 let b=23
 a=a+b 
 
+let nombre = "angelica" 
+let apellido = "cisneros" 
+
+let nombreCompleto = nombre + " " + apellido 
+
+console.log(nombreCompleto)
+
+let nickname =`mi nombre es ${nombre} y mi apellido es ${cisneros}`
+console.log(nickname)
