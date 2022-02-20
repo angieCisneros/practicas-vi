b7567a4d9abd376308f40b8d2fd5fba2807a323e
¨Angelica Cisneros <angie.cisneros.gomez@gmail.com>
2022-02-19 21:40:26 -0600

    practicas de js

--- practica.js
+++ practica.js
@@ -14,3 +14,30 @@ console.log(nombreCompleto)
 
 let nickname =`mi nombre es ${nombre} y mi apellido es ${cisneros}`
 console.log(nickname)
+
+let a = 60
+let b = 20
+
+let resultado = a == b // triple === es estrictamente igual
+
+console.log(resultado)
+
+let c = 37
+let d = 30 
+
+let resuldado = c != d  // !== es estrictamente diferente
+
+console.log(resuldado)
+
+let e = 55
+let f = 44
+
+console.log(e < f) // <= es menor o igual que y >= es mayor o igual que 
+
+let g = 10 
+let h = 15
+let i = 20
+
+console.log(g > h && g < i) // && es "a su vez" || es "o"
+// let resp = (g > h) && (g < i)
+
